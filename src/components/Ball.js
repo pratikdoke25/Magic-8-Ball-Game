e7f4224be1423a8fb54bf790f45b605f5cc033e4
Robby Renz <nxgrenz@gmail.com>
2019-11-22 18:07:27 -0600

    refreshing the page renders an 8-ball answer

--- src/components/Ball.js
+++ src/components/Ball.js
@@ -4,16 +4,22 @@ class Ball extends React.Component {
     constructor() {
         super()
         this.state = {
-            answers: null,
+            answers: ['It is certain', 'Without a doubt', 'You may rely on it', 'Yes definitely', 'It is decidedly so', 'As I see it, yes', 'Most likely', 'Yes', 'Outlook good', 'Signs point to yes', 'Reply hazy try again', 'Better not tell you now', 'Ask again later', 'Cannot predict now', 'Concentrate and ask again', 'Don’t count on it', 'Outlook not so good', 'My sources say no', 'Very doubtful', 'My reply is no'],
             currentAnswer: null
         }
+    }
+
+    componentDidMount() {
+        const index = Math.floor(Math.random() * this.state.answers.length)
+        this.setState({ currentAnswer: this.state.answers[index] })
     }
 
     render() {
         return (
             <div className="ball">
-                <h1>Magic 8-Ball</h1>
-                <h2>Press the <kbd>spacebar</kbd> for an answer...</h2>
+                {/* <h1>Magic 8-Ball</h1>
+                <h2>Press the <kbd>spacebar</kbd> for an answer...</h2> */}
+                <h1>{this.state.currentAnswer}</h1>
             </div>
         )
     }
